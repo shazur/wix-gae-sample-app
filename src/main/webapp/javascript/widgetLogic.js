4d246b64e133d286c291f3e0bb0c378c11a226ac
Shahar Zur <shaharz@wix.com>
2012-09-12 10:00:53 -0400

    started working on functions with parameters

--- src/main/webapp/javascript/widgetLogic.js
+++ src/main/webapp/javascript/widgetLogic.js
@@ -1,30 +1,3 @@
-function getAllMethods() {
-    return Object.getOwnPropertyNames(Wix).filter(function(property) {
-        return typeof Wix[property] == 'function';
-    });
-}
-
-function isScriptLoaded() {
-   if (!window['Wix']) {
-        $("#resultContent").text("Please choose API version first!");
-        return false;
-    }
-    return true;
-}
-
-function addFunctionToContainer(func) {   
-    var functionSection = '<hr/>' +
-        '<div class="row-fluid">' +
-            '<div id="funcName" class="pull-left">' + func + '</div>' +
-            '<button id="run' + func + '"' + 'class="btn pull-right">Run</button>'
-            '<div id="result" class="hidden"></div>' +
-        '</div>' +
-    '<hr/>'
-    
-    $("#functionsContainer").append(functionSection);
-}
-
-
 $(document).ready(function() {
     
     $("#version").change(function(ev) {
@@ -54,23 +27,60 @@ $(document).ready(function() {
         addListenersToRunButtons(sdkFunctions);
     });
     
-    function getFuncParameters(funcName) {
-        return null;
+    var parametersValueMap = {
+        addEventListener: {eventName: "Wix.Events.PAGE_NAVIGATION_CHANGE",
+                            callBack: "function(data){$(\"#resultContent\").html(\"<pre>\" + JSON.stringify(data, null, '  ') + \"</pre>\")};"
+                          },
+        getSiteInfo:      {onSuccess: "function(data){$(\"#resultContent\").html(\"<pre>\" + JSON.stringify(data, null, '  ') + \"</pre>\")};"}    
     }
     
+    function getAllMethods() {
+    return Object.getOwnPropertyNames(Wix).filter(function(property) {
+        return typeof Wix[property] == 'function';
+    });
+}
+
+    function getFuncParameters(funcName) {    
+        var func = Wix[funcName].toString();
+        return func.slice(func.indexOf('(')+1, func.indexOf(')')).match(/([^\s,]+)/g);
+    }
+    
+    function isScriptLoaded() {
+       if (!window['Wix']) {
+            $("#resultContent").text("Please choose API version first!");
+            return false;
+        }
+        return true;
+    }
+    
+    function addFunctionToContainer(func) {   
+        var functionSection = '<hr/>' +
+            '<div class="row-fluid">' +
+                '<div id="funcName" class="pull-left">' + func + '</div>' +
+                '<button id="run' + func + '"' + 'class="btn pull-right">Run</button>'
+                '<div id="result" class="hidden"></div>' +
+            '</div>' +
+        '<hr/>'
+        
+        $("#functionsContainer").append(functionSection);
+    }
     
     function addListenersToRunButtons(functions) {
-        functions.forEach(function(func) {
-            $("#run"+func).click({funcName: func}, function(event) {
-                if (isScriptLoaded()) {
-                    var funcName = event.data.funcName;
-                    var parameters = getFuncParameters(funcName);
-                    Wix[funcName](parameters);
-                }
+            functions.forEach(function(func) {
+                $("#run"+func).click({funcName: func}, function(event) {
+                    if (isScriptLoaded()) {
+                        var funcName = event.data.funcName;
+                        var parameters = getFuncParameters(funcName);
+                        var parameterValues = [];
+                        for (var i=0; i<parameters.length; i++) {
+                             parameterValues[i] = parametersValueMap[funcName][parameters[i]];
+                        }
+                        Wix[funcName].apply(Wix, parameterValues);                        
+                    }
+                });
+                
             });
-            
-        });
-    }
+        }
 
     
     $("#runA").click(function() {
